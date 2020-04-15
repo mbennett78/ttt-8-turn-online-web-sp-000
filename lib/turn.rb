5973f6ebff7ae775abc100570679377e7abcd1af
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-04-15 20:10:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-
+# This method creates the layout of the board and accepts an array which will be the game board's positions or state
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,50 +8,49 @@ def display_board(board)
 end
 
 
-
+# This method will convert the input from the user to an index on the board since index starts counting from 0
 def input_to_index(input)
-  position = (input.to_i) -1
-  return position
+   index = input.to_i -1
 end
 
 
-def position_taken?(board,answer)
-     answer = answer.to_i
+#This method describes what must change in order to make a move
+# In order to make a move, you must have the state of the board, the user selects a location as input, and applies their token to that spot
+def move(board, input, token="X")
+  index = input_to_index(input)
+  board[index] = token
+end
 
-     if board [answer] == "X" || board[answer] =="O"
-     puts "That position is taken."
-       return true
-     else
-        return false
-     end
+#This method will look at the board and go to the input to see if the position already has a token of X or O
+def position_taken?(board,input)
+  index = input_to_index(input)
+  if board[index] != " "
+    return true
+  else 
+    return false
+  end
 end
 
 
+#This method needs to make sure the move is valid by making sure the input is 1-9 and the position is open
 def valid_move?(board, number_entered)
-
-number_entered = input_to_index(number_entered)
-
- number_entered.between?(0, 8) && !(position_taken?(board,number_entered))
-
+   index = input_to_index(number_entered)
+   index.between?(0,8) && !position_taken?(board,number_entereed)
 end
 
 
-def move (array, position, char="X")
-  array [position] = char
-end
-
-
+#This method allows the user to take a turn playing the game
 def turn(board)
-  puts "Please enter a token X or O:"
-  token = gets.chomp
-  puts "Please enter 1-9:"
+  puts "Please enter a move 1-9:"
   answer = gets.chomp
-  index = input_to_index(answer)
-   if valid_move?(board,answer)
-     move(board,index,token)
-     display_board(board)
+  puts "Please enter your token X or O:"
+  token = gets.chomp
+  if valid_move?(board, answer)
+      move (board,answer, token)
+      display_board(board)
   else
-    puts "That is an invalid entry!"
+    puts "This is an invalid move."
     turn(board)
   end
 end
+   
