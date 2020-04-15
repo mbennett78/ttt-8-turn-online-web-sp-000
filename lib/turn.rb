69bf8f0f2c0e2b6f66606671e7b7ab373ae964d2
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-04-15 20:53:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ end
 
 #This method needs to make sure the move is valid by making sure the input is 1-9 and the position is open
 def valid_move?(board,input)
-  
+
    index = input_to_index(input)
    if (index >= 0 && index <= 8) && position_taken?(board,number_entered)!=true
      return true
